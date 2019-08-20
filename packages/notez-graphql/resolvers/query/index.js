81484693ef4be4af899e0ccde037a9d7a86e116c
Talysson <talyssonoc@gmail.com>
2019-08-20 16:00:26 -0300

    Remove notez-graphql files from inside of src folder

--- packages/notez-graphql/resolvers/query/index.js
+++ packages/notez-graphql/resolvers/query/index.js
@@ -1,5 +1,5 @@
 const { pascalCase } = require('change-case')
-const { workspaceFactory } = require('../../../../notez-domain/workspace')
+const { workspaceFactory } = require('../../../notez-domain/workspace')
 
 exports.Query = {
   defaultWorkspace: async (p, _, { container }) => {
