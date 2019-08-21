c6ab77229f1a2352b1c8c92436fddf5ab617db23
Talysson <talyssonoc@gmail.com>
2019-08-21 15:51:26 -0300

    Add tests to userRepository#fromAuth

--- packages/notez-infra/user/userRepository.test.js
+++ packages/notez-infra/user/userRepository.test.js
@@ -34,22 +34,78 @@ describe('User :: userRepository', () => {
         await expect(userRepository.getById(newUser.id)).resolves.toBeTruthy()
       })
     })
+
+    describe('when there is a user with given email already', () => {
+      it('fails and throws already in use error', async () => {
+        const user = {
+          name: 'User',
+          email: 'user@email.com',
+          password: '12345',
+        }
+
+        await userRepository.add(user)
+
+        await expect(userRepository.add(user)).rejects.toHaveProperty(
+          'code',
+          'EMAIL_ALREADY_IN_USE'
+        )
+      })
+    })
   })
 
-  describe('when there is a user with given email already', () => {
-    it('fails and throws already in use error', async () => {
-      const user = {
-        name: 'User',
-        email: 'user@email.com',
-        password: '12345',
-      }
+  describe('#fromAuth', () => {
+    describe('when user exists', () => {
+      describe('when password is right', () => {
+        it('returns the user', async () => {
+          const user = await userRepository.add({
+            name: 'User',
+            email: 'user@email.com',
+            password: '12345',
+          })
 
-      await userRepository.add(user)
+          const userPromise = userRepository.fromAuth({
+            email: 'user@email.com',
+            password: '12345',
+          })
 
-      await expect(userRepository.add(user)).rejects.toHaveProperty(
-        'code',
-        'EMAIL_ALREADY_IN_USE'
-      )
+          await expect(userPromise).resolves.toHaveProperty('id', user.id)
+          await expect(userPromise).resolves.toHaveProperty('email', user.email)
+        })
+      })
+
+      describe('when password is wrong', () => {
+        it('fails and throws not found error', async () => {
+          await userRepository.add({
+            name: 'User',
+            email: 'user@email.com',
+            password: '12345',
+          })
+
+          const userPromise = userRepository.fromAuth({
+            email: 'user@email.com',
+            password: '54321',
+          })
+
+          await expect(userPromise).rejects.toHaveProperty(
+            'code',
+            'USER_NOT_FOUND'
+          )
+        })
+      })
+    })
+
+    describe('when user does not exist', () => {
+      it('fails and throws not found error', async () => {
+        const userPromise = userRepository.fromAuth({
+          email: 'user@email.com',
+          password: '54321',
+        })
+
+        await expect(userPromise).rejects.toHaveProperty(
+          'code',
+          'USER_NOT_FOUND'
+        )
+      })
     })
   })
 })
