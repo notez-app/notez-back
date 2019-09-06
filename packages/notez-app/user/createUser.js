ad9853a4cc205ba3e04590975306d2e9c05d4c32
Talysson <talyssonoc@gmail.com>
2019-09-06 10:51:24 -0300

    Update structure in order to use custom strict mode error

--- packages/notez-app/user/createUser.js
+++ packages/notez-app/user/createUser.js
@@ -2,13 +2,11 @@ const { User } = require('@notez/domain/user')
 
 module.exports = ({ createUnitOfWork, userRepository }) =>
   async function createUser(userData) {
-    const user = new User({
+    const user = User.buildStrict({
       name: userData.name,
       email: userData.email,
       password: userData.password,
     })
-
-    user.assertValidity()
 
     return await createUnitOfWork(async () => {
       return await userRepository.add(user)
--- packages/notez-domain/user/User.js
+++ packages/notez-domain/user/User.js
@@ -1,21 +1,16 @@
 const { attributes } = require('structure')
 const InvalidUserError = require('./errors/InvalidUserError')
 
-const User = attributes({
-  id: { type: Number },
-  name: { type: String, required: true },
-  email: { type: String, required: true, email: true },
-  password: { type: String, required: true },
-})(
-  class User {
-    assertValidity() {
-      const { valid, errors } = this.validate()
-
-      if (!valid) {
-        throw new InvalidUserError(errors)
-      }
-    }
+const User = attributes(
+  {
+    id: { type: Number },
+    name: { type: String, required: true },
+    email: { type: String, required: true, email: true },
+    password: { type: String, required: true },
+  },
+  {
+    strictValidationErrorClass: InvalidUserError,
   }
-)
+)(class User {})
 
 module.exports = User
